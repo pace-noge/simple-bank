ca6657dab6cde5abd9954f5689415b187817393f
Nasaruddin <nasaruddin@mekari.com>
2023-01-22 21:48:09 +0700

    Backend 17 How to securely store password

--- api/server.go
+++ api/server.go
@@ -30,6 +30,9 @@ func NewServer(store db.Store) *Server {
 	// transfers
 	router.POST("/transfers", server.createTransfer)
 
+	// users
+	router.POST("/users", server.createUser)
+
 	// add router
 	server.router = router
 	return server
--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomStr(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
